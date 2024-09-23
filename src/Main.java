b44b09ff7cd04a5e9067f844ead164a26c576cfa
Bandita Chalise <chalisba@mail.uc.edu>
2024-09-23 10:17:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number % 2 == 0) {
+            System.out.println("The number "+ number+ " is Even");
+        }
+        else {
+            System.out.println("The number "+ number+ " is Odd");
+        }
+    }
+}
